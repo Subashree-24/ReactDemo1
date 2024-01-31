08f28d37e8b09e770a2c4f25852d76a8dedc50b8
Subashree-24 <142602553+Subashree-24@users.noreply.github.com>
2024-01-31 23:02:09 +0530

    Add files via upload

---
+++ src/firstone.js
@@ -0,0 +1,14 @@
+import "./firststyles.css";
+
+function DemoDogs() {
+  return (
+    <div className="App">
+      <h1>Hello Guys</h1>
+      <h2>
+        Welcome 
+      </h2>
+    </div>
+  );
+}
+
+export default DemoDogs;
